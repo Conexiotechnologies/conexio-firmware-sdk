d2f0d11706870c073d149842803719c918a98ab5
rajeev1986 <rajeev.piyare@hotmail.com>
2021-11-03 21:19:08 -0500

    Sensirion SHT4x sensor sample

---
+++ samples/sht4x/README.rst
@@ -0,0 +1,49 @@
+.. _sht4x:
+
+SHT4X: High accuracy digital I2C humidity and temperature sensor
+####################################################################################
+
+Description
+***********
+
+This sample application periodically measures the ambient temperature and humidity
+from an SHT4X sensor onboard Conexio Stratus kit. The result is written to the console.
+
+The SHT4X has the option to use a heater which makes sense for specific
+environments/applications (refer to the datasheet for more information).
+To make use of the heater have a look at the Kconfig options for this application.
+
+
+References
+**********
+
+ - `SHT4X sensor <https://www.sensirion.com/en/environmental-sensors/humidity-sensors/humidity-sensor-sht4x/>`_
+
+Wiring
+******
+
+This sample uses the SHT4X sensor controlled using the I2C interface of the Conexio Stratus shield.
+
+Building and Running
+********************
+
+This project outputs sensor data to the console. It should work with any platform featuring a I2C peripheral
+interface.
+
+.. zephyr-app-commands::
+   :zephyr-app: samples/sensor/sht4x
+   :board: conexio_stratus_ns
+   :goals: build flash
+
+
+Sample Output
+=============
+
+.. code-block:: console
+
+        Conexio Stratus SHT4X sensor example
+        SHT4X: 23.64 Temp. [C] ; 30.74 RH [%]
+        SHT4X: 23.66 Temp. [C] ; 32.16 RH [%]
+        SHT4X: 23.63 Temp. [C] ; 30.83 RH [%]
+
+      <repeats endlessly every 2 seconds>
---
+++ samples/sht4x/src/main.c
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2021 Leonard Pollak
+ * Copyright (c) 2021 Conexio Technologies, Inc
+ * 
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/sensor.h>
+#include <stdio.h>
+
+#include <drivers/sensor/sht4x.h>
+
+#if !DT_HAS_COMPAT_STATUS_OKAY(sensirion_sht4x)
+#error "No sensirion,sht4x compatible node found in the device tree"
+#endif
+
+void main(void)
+{
+	printf("Conexio Stratus SHT4X sensor example\n");
+
+	const struct device *sht = DEVICE_DT_GET_ANY(sensirion_sht4x);
+	struct sensor_value temp, hum;
+
+	if (!device_is_ready(sht)) {
+		printf("Device %s is not ready.\n", sht->name);
+		return;
+	}
+
+#if CONFIG_APP_USE_HEATER
+	struct sensor_value heater_p;
+	struct sensor_value heater_d;
+
+	heater_p.val1 = CONFIG_APP_HEATER_PULSE_POWER;
+	heater_d.val1 = CONFIG_APP_HEATER_PULSE_DURATION;
+	sensor_attr_set(sht, SENSOR_CHAN_ALL,
+			SENSOR_ATTR_SHT4X_HEATER_POWER, &heater_p);
+	sensor_attr_set(sht, SENSOR_CHAN_ALL,
+			SENSOR_ATTR_SHT4X_HEATER_DURATION, &heater_d);
+#endif
+
+	while (true) {
+
+		if (sensor_sample_fetch(sht)) {
+			printf("Failed to fetch sample from SHT4X device\n");
+			return;
+		}
+
+		sensor_channel_get(sht, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		sensor_channel_get(sht, SENSOR_CHAN_HUMIDITY, &hum);
+#if CONFIG_APP_USE_HEATER
+		/*
+		 * Conditions in which it makes sense to activate the heater
+		 * are application/environment specific.
+		 *
+		 * The heater should not be used above SHT4X_HEATER_MAX_TEMP (65 °C)
+		 * as stated in the datasheet.
+		 *
+		 * The temperature data will not be updated here for obvious reasons.
+		 **/
+		if (hum.val1 > CONFIG_APP_HEATER_HUMIDITY_THRESH &&
+				temp.val1 < SHT4X_HEATER_MAX_TEMP) {
+			printf("Activating heater.\n");
+
+			if (sht4x_fetch_with_heater(sht)) {
+				printf("Failed to fetch sample from SHT4X device\n");
+				return;
+			}
+
+			sensor_channel_get(sht, SENSOR_CHAN_HUMIDITY, &hum);
+		}
+#endif
+
+		printf("SHT4X: %.2f Temp. [C] ; %0.2f RH [%%]\n",
+		       sensor_value_to_double(&temp),
+		       sensor_value_to_double(&hum));
+
+		/* Maximum duty cycle for using the heater is 5% */
+#if CONFIG_APP_USE_HEATER && CONFIG_APP_HEATER_PULSE_DURATION == 0
+		k_sleep(K_MSEC(20000));
+#else
+		k_sleep(K_MSEC(2000));
+#endif
+	}
+}
