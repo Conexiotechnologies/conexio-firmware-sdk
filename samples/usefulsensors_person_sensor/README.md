b71c7169983d6ff7c90e31aae806875a5460890a
rajeev1986 <rajeev.piyare@hotmail.com>
2022-12-15 20:57:07 -0600

    sample application to get started with useful person sensor

---
+++ samples/usefulsensors_person_sensor/README.md
@@ -0,0 +1,39 @@
+# Conexio Stratus + Useful Sensors Person Sensor
+
+This repository contains example code for communicating with the Useful Sensors Person Sensor over I2C
+for face detection.
+
+Wiring Information
+=============
+Connect the sensor breakout board to the Conexio Stratus shield using Qwiic or Grove
+connector.
+
+Building and Running on conexio_stratus
+===========================
+To compile te application, execute:
+```
+west build -b conexio_stratus_ns
+```
+
+Flash the compiled firmware using `newtmgr`:
+```
+newtmgr -c serial image upload build/zephyr/app_update.bin
+```
+
+Sample Output
+=============
+```
+*** Booting Zephyr OS build v2.6.99-ncs1  ***
+[00:00:00.205,596] <inf> app: Usefulsensors person sensor sample started
+[00:00:00.205,627] <inf> app: Setting up i2c
+[00:00:00.205,627] <inf> app: I2C device OK
+[00:00:03.209,808] <inf> app: ****************
+[00:00:03.209,808] <inf> app: 1 face(s) found
+[00:00:03.209,838] <inf> app: Face #0: 69 confidence, (100, 56), (126, 108), not facing
+[00:00:05.211,944] <inf> app: ****************
+[00:00:05.211,944] <inf> app: 1 face(s) found
+[00:00:05.211,975] <inf> app: Face #0: 94 confidence, (49, 19), (101, 99), facing
+[00:00:06.213,012] <inf> app: ****************
+[00:00:06.213,043] <inf> app: 1 face(s) found
+[00:00:06.213,043] <inf> app: Face #0: 69 confidence, (98, 58), (128, 106), facing
+```
---
+++ samples/usefulsensors_person_sensor/src/main.c
@@ -0,0 +1,118 @@
+/*---------------------------------------------------------------------------*/
+/*
+ * Copyright (c) 2022 Conexio Technologies, Inc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */   
+/*---------------------------------------------------------------------------*/
+#include <zephyr.h>
+#include <stdio.h>
+#include <device.h>
+#include <drivers/i2c.h>
+#include <drivers/sensor.h>
+#include "person_sensor.h"
+#include <logging/log.h>
+/*---------------------------------------------------------------------------*/
+/* Register the logging module */
+LOG_MODULE_REGISTER(app, CONFIG_USEFULSENSORS_LOG_LEVEL);
+/*---------------------------------------------------------------------------*/
+const struct device * i2c_device;
+#define I2C_DEV "I2C_1"
+/*---------------------------------------------------------------------------*/
+static int rc;
+static int i;
+/*---------------------------------------------------------------------------*/
+// How long to wait between sensor sampling. The sensor can be read as
+// frequently as you like, but the results only change at about 5FPS, so
+// waiting for >200ms is reasonable. Here, we are waiting for 1 sec. 
+#define SAMPLING_INTERVAL_MS   (K_MSEC(1000))
+/*---------------------------------------------------------------------------*/
+/*
+ * Brief : Fetch the latest results from the sensor
+ * Param ; Pointer to the results
+ * Return : false if the read didn't succeed.
+ */
+bool person_sensor_read(person_sensor_results_t* results) {
+    rc = i2c_read(i2c_device, (uint8_t*)(results), sizeof(person_sensor_results_t), PERSON_SENSOR_I2C_ADDRESS);
+    if (rc < 0) {
+        LOG_ERR("i2c_read error: %d", rc);
+        return false;
+	} else {
+        return true;
+    }
+}
+/*---------------------------------------------------------------------------*/
+/*
+ * Brief : Writes the value to the sensor register
+ * Param1 : Register address
+ * Param2 : values to be written
+ * Return : void
+ */
+void person_sensor_write_reg(uint8_t reg, uint8_t value) {
+    uint8_t write_bytes[2] = {reg, value};
+
+    rc = i2c_write(i2c_device, write_bytes, sizeof(write_bytes), PERSON_SENSOR_I2C_ADDRESS);
+    if (rc < 0) {
+        LOG_ERR("i2c_write error: %d", rc);
+	}
+}
+/*---------------------------------------------------------------------------*/
+/*
+ * Brief : Initialize the I2C bus
+ * Param : I2C device 
+ * Return : -EINVAL if failed, else 0
+ */
+static int i2c_init(void) {
+    LOG_INF("Setting up i2c");
+    
+    i2c_device = device_get_binding(I2C_DEV);
+    if (i2c_device == NULL) 
+    {
+        LOG_ERR("Failed to get I2C device");
+        return -EINVAL;
+    }
+
+    int ret = i2c_configure(i2c_device, I2C_SPEED_SET(I2C_SPEED_FAST));
+    if (ret < 0 )
+    {
+        LOG_ERR("I2C Configuration failed");
+        return ret;
+    }
+
+    return 0;
+}
+/*---------------------------------------------------------------------------*/
+void main(void) 
+{
+	LOG_INF("Usefulsensors person sensor sample started"); 
+    person_sensor_results_t results = {};
+
+    i2c_init();
+    
+    while (1)
+    {
+        // Perform a read action on the I2C address of the sensor to get the
+        // current face information detected.
+        if (!person_sensor_read(&results)) 
+        {
+            LOG_ERR("No person sensor results found on the i2c bus");
+            k_sleep(SAMPLING_INTERVAL_MS);
+            return;
+        }
+
+        LOG_INF("****************");
+        LOG_INF("%d face(s) found", results.num_faces);
+
+        for (i = 0; i < results.num_faces; ++i) 
+        {
+            const person_sensor_face_t* face = &results.faces[i];
+
+            LOG_INF("Face #%d: %d confidence, (%d, %d), (%d, %d), %s",
+            i, face->box_confidence, face->box_left, face->box_top,
+            face->box_right, face->box_bottom, 
+            face->is_facing ? "facing" : "not facing");
+        }
+        k_sleep(SAMPLING_INTERVAL_MS);
+    }
+}
+/*---------------------------------------------------------------------------*/
